9e31378bfe377e22b639fd791ff798d21a212c82
Payalvaja <payalvaja80@gmail.com>
2025-06-26 18:41:19 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,35 @@
 #include<iostream>
-#include"calculator.h"
+#include<calculator.h>
 
 using namespace std;
 int main(){
-  void calculator();
-   char op;
-   int num1=10;
-   int num2=5;
-   int result;
+char op;
+int num1,num2,result;
 
-cout<<"Enter an op(+,-,*,/):";
+cout<<"enter an op(+,-,*,/):";
 cin>>op;
+cout<<"enter two value";
+cin>>num1>>num2;
 
 switch(op){
- case'+':
-   cout<<"result:"<<num1<<"+"<<num2<<"="<<result<<endl;
-   break;
- case'-':
-   cout<<"result:"<<num1<<"-"<<num2<<"="<<result<<endl;
-   break;
-case'*':
-   cout<<"result:"<<num1<<"*"<<num2<<"="<<result<<endl;
-   break;
-case'/':
-   cout<<"result:"<<num1<<"/"<<num2<<"="<<result<<endl;
-   break;
+case '+':
+ result = num1+num2;
+cout<<"result:"<<num1<<"+"<<num2<<"="<<result<<endl;
+break;
+case '-':
+result = num1-num2;
+cout<<"result:"<<num1<<"-"<<num2<<"="<<result<<endl;
+break;
+case '*':
+result = num1*num2;
+cout<<"result:"<<num1<<"*"<<num2<<"="<<result<<endl;
+break;
+case '/':
+result = num1/num2;
+cout<<"result:"<<num1<<"/"<<num2<<"="<<result<<endl;
+break;
 default:
-cout<<"Enter a valid number"<<endl;
+cout<<"enter valid numbers";
 }
 return 0;
 }
