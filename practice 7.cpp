190aea12d5ceb2fb1390631f56832476241bcb57
Payalvaja <payalvaja80@gmail.com>
2025-07-05 21:25:18 +0530

    Create practice 7.cpp

---
+++ practice 7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int value;
+cout<<"enter value:"<<endl;
+cin>>value;
+if(value>0){
+cout<<"the value is positive"<<endl;
+}
+else if(value<0){
+cout<<"the value is nagative"<<endl;
+}
+else{
+cout<<"the number is zero"<<endl;
+}
+return 0;
+}
