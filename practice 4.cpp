eac7e4bebadc5a24f8b4fbf106e606f5072aac1c
Payalvaja <payalvaja80@gmail.com>
2025-07-12 09:41:39 +0530

    Update practice 4.cpp

--- practice 4.cpp
+++ practice 4.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
-#include<limits>
-using namespace std;
-int main(){
-cout<<"the number of min"<<numeric_limits<int>::min()<<endl;
-cout<<"the number of max"<<numeric_limits<int>::max()<<endl;
-return 0;
-}
+ #include <iostream> 
+#include <climits>  
+using namespace std; // Using the standard namespace
+
+int main() 
+{
+    cout << "\n\n Check the upper and lower limits of integer :\n"; // Outputting a message for checking integer limits
+	cout << "--------------------------------------------------\n"; // Outputting a separator line
+
+	// Outputting the maximum and minimum limits of various data types
+	cout << " The maximum limit of int data type :" << INT_MAX << endl;
+	cout << " The minimum limit of int data type :" << INT_MIN << endl;
+	cout << " The maximum limit of unsigned int data type :" << UINT_MAX << endl;
+	cout << " The maximum limit of long long data type :" << LLONG_MAX << endl;
+	cout << " The minimum limit of long long data type :            " << LLONG_MIN << endl;
+	cout << " The maximum limit of unsigned long long data type :   " << ULLONG_MAX << endl;
+	cout << " The Bits contain in char data type :                  " << CHAR_BIT << endl;
+	cout << " The maximum limit of char data type :                 " << CHAR_MAX << endl;
+	cout << " The minimum limit of char data type :                 " << CHAR_MIN << endl;
+	cout << " The maximum limit of signed char data type :          " << SCHAR_MAX << endl;
+	cout << " The minimum limit of signed char data type :          " << SCHAR_MIN << endl;
+	cout << " The maximum limit of unsigned char data type :        " << UCHAR_MAX << endl;
+	cout << " The minimum limit of short data type :                " << SHRT_MIN << endl;
+    cout << " The maximum limit of short data type :                " << SHRT_MAX << endl;
+    cout << " The maximum limit of unsigned short data type :       " << USHRT_MAX << endl;
+
+    cout << endl; // Outputting a blank line for better readability
+
+    return 0; // Returning 0 to indicate successful program execution
+} // End of the main function
