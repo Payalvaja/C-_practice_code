5cc4503a653a0d65d996a1a3c3bd13c5126b7093
Payalvaja <payalvaja80@gmail.com>
2025-07-13 14:55:18 +0530

    Update practice 2.cpp

--- practice 2.cpp
+++ practice 2.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 using namespace std;
 int add(int x,int y){
-return a+b;
+return x+y;
 }
 int main(){
-int x,y;
 cout<<"enter first value ";
 cin>>x;
 
 cout<<"enter second value";
 cin>>y;
-cout<<"sum:"<<add(x,y)<<endl;
+cout<<x<<"+"<<y<<"="<<endl;
 return 0;
 }
