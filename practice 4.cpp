bbde05a2a66a781f705bf2ff4b8ace71956faf14
Payalvaja <payalvaja80@gmail.com>
2025-07-15 08:32:39 +0530

    Update practice 4.cpp

--- practice 4.cpp
+++ practice 4.cpp
@@ -1,27 +1,26 @@
- #include <iostream> 
-#include <climits>  
-using namespace std; 
+ #include <iostream>
+using namespace std;
 
-int main() 
-{
-	
-	cout << " The maximum limit of int data type :" << INT_MAX << endl;
-	cout << " The minimum limit of int data type :" << INT_MIN << endl;
-	cout << " The maximum limit of unsigned int data type :" << UINT_MAX << endl;
-	cout << " The maximum limit of long long data type :" << LLONG_MAX << endl;
-	cout << " The minimum limit of long long data type :            " << LLONG_MIN << endl;
-	cout << " The maximum limit of unsigned long long data type :   " << ULLONG_MAX << endl;
-	cout << " The Bits contain in char data type :                  " << CHAR_BIT << endl;
-	cout << " The maximum limit of char data type :                 " << CHAR_MAX << endl;
-	cout << " The minimum limit of char data type :                 " << CHAR_MIN << endl;
-	cout << " The maximum limit of signed char data type :          " << SCHAR_MAX << endl;
-	cout << " The minimum limit of signed char data type :          " << SCHAR_MIN << endl;
-	cout << " The maximum limit of unsigned char data type :        " << UCHAR_MAX << endl;
-	cout << " The minimum limit of short data type :                " << SHRT_MIN << endl;
-    cout << " The maximum limit of short data type :                " << SHRT_MAX << endl;
-    cout << " The maximum limit of unsigned short data type :       " << USHRT_MAX << endl;
+int main() {
+    int num[3]; // Array with 3 elements
 
-    cout << endl; 
+    cout << "Enter three numbers: ";
+    for (int i = 0; i < 3; i++) {
+        cin >> num[i]; // Taking input in array
+    }
 
-    return 0; 
-} 
+    int max = num[0]; // Assume first as max
+    int min = num[0]; // Assume first as min
+
+    for (int i = 1; i < 3; i++) {
+        if (num[i] > max)
+            max = num[i];
+        if (num[i] < min)
+            min = num[i];
+    }
+
+    cout << "Maximum number is: " << max << endl;
+    cout << "Minimum number is: " << min << endl;
+
+    return 0;
+}
